45bd5e1896528204d954b758580a3e5b0132a084
yume <786659519@qq.com>
2019-04-26 10:00:16 +0800

    2222222

---
+++ XUnitTestProject5/XUnitTestProject5/ApiHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace XUnitTestProject5
+{
+    class Apihelper
+    {
+        public Apihelper() { }
+
+
+        public void  Get() {
+
+
+        }
+        public  void Post() {
+        }
+
+        public void Delete() {
+        }
+
+        public void Put() {
+        }
+
+
+
+
+
+
+
+    }
+}
